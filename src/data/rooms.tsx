5d35bf1b3a8eb2284804f281daed6768e88a40dd
Gung Nanda <gungnands@gmail.com>
2025-11-17 19:12:48 +0800

    constants

---
+++ src/data/rooms.tsx
@@ -0,0 +1,44 @@
+const rooms = [
+    {
+        id: '1',
+        image: require('../assets/images/costher.png'),
+        price: 'Rp 2.200.000 / bulan',
+        facilities: ['AC', 'WiFi', 'Kamar Mandi Dalam', 'Lantai 1', 'Fully Furnished'],
+        number: 'Kamar 01',
+        waterUsage: '50',
+        electricityUsage: '130',
+        available: true,
+    },
+    {
+        id: '2',
+        image: require('../assets/images/costher.png'),
+        price: 'Rp 1.200.000 / bulan',
+        facilities: ['Kipas Angin', 'WiFi', 'Kamar Mandi Luar', 'Lantai 1', 'Semi Furnished'],
+        number: 'Kamar 02',
+        waterUsage: '40',
+        electricityUsage: '100',
+        available: false,
+    },
+    {
+        id: '3',
+        image: require('../assets/images/costher.png'),
+        price: 'Rp 2.200.000 / bulan',
+        facilities: ['AC', 'Meja Belajar', 'Kamar Mandi Dalam', 'Lantai 1', 'Fully Furnished'],
+        number: 'Kamar 03',
+        waterUsage: '55',
+        electricityUsage: '120',
+        available: true,
+    },
+    {
+        id: '4',
+        image: require('../assets/images/costher.png'),
+        price: 'Rp 2.200.000 / bulan',
+        facilities: ['AC', 'Meja Belajar', 'Kamar Mandi Dalam', 'Lantai 1', 'Fully Furnished'],
+        number: 'Kamar 04',
+        waterUsage: '60',
+        electricityUsage: '125',
+        available: true,
+    },
+];
+
+export default rooms;
