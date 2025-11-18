f85edc868c554dd74e6821c40c1c4c5658b25e17
Gung Nanda <gungnands@gmail.com>
2025-11-19 00:20:47 +0800

    safe area view

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Platform } from "react-native";
+
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import colors from "../styles/colors";
 
@@ -7,81 +8,84 @@ import LoginLogo from "../components/Login/LoginLogo";
 import LoginTitle from "../components/Login/LoginTitle";
 import LoginInput from "../components/Login/LoginInput";
 import LoginButton from "../components/Login/LoginButton";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     return (
-        <KeyboardAwareScrollView
-            contentContainerStyle={{
-                flexGrow: 1,
-                justifyContent: "center",
-                paddingHorizontal: 24,
-                paddingVertical: 40,
-            }}
-            enableOnAndroid={true}
-            extraScrollHeight={Platform.OS === "ios" ? 20 : 80}
-            keyboardOpeningTime={0}
-        >
-            <LoginLogo />
-            <LoginTitle />
-
-            <View style={{ gap: 16 }}>
-                <LoginInput label="Email" value={email} onChange={setEmail} />
-                <LoginInput
-                    label="Password"
-                    value={password}
-                    onChange={setPassword}
-                    secure
-                />
-            </View>
-
-            <TouchableOpacity style={{ alignSelf: "flex-end", marginTop: 10 }}>
-                <Text
-                    style={{
-                        fontFamily: "Inter-Medium",
-                        fontSize: 13,
-                        color: colors.deepMaroon,
-                    }}
-                >
-                    Lupa Password?
-                </Text>
-            </TouchableOpacity>
-
-            <LoginButton onPress={() => console.log("Login pressed")} />
-
-            <View
-                style={{
-                    flexDirection: "row",
+        <SafeAreaView >
+            <KeyboardAwareScrollView
+                contentContainerStyle={{
+                    flexGrow: 1,
                     justifyContent: "center",
-                    marginTop: 20,
+                    paddingHorizontal: 24,
+                    paddingVertical: 40,
                 }}
+                enableOnAndroid={true}
+                extraScrollHeight={Platform.OS === "ios" ? 20 : 80}
+                keyboardOpeningTime={0}
             >
-                <Text
-                    style={{
-                        fontFamily: "Inter-Regular",
-                        fontSize: 13,
-                        color: colors.darkCharcoal,
-                    }}
-                >
-                    Belum punya akun?
-                </Text>
+                <LoginLogo />
+                <LoginTitle />
 
-                <TouchableOpacity>
+                <View style={{ gap: 16 }}>
+                    <LoginInput label="Email" value={email} onChange={setEmail} />
+                    <LoginInput
+                        label="Password"
+                        value={password}
+                        onChange={setPassword}
+                        secure
+                    />
+                </View>
+
+                <TouchableOpacity style={{ alignSelf: "flex-end", marginTop: 10 }}>
                     <Text
                         style={{
                             fontFamily: "Inter-Medium",
                             fontSize: 13,
                             color: colors.deepMaroon,
-                            marginLeft: 4,
                         }}
                     >
-                        Daftar Sekarang
+                        Lupa Password?
                     </Text>
                 </TouchableOpacity>
-            </View>
-        </KeyboardAwareScrollView>
+
+                <LoginButton onPress={() => console.log("Login pressed")} />
+
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        marginTop: 20,
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontFamily: "Inter-Regular",
+                            fontSize: 13,
+                            color: colors.darkCharcoal,
+                        }}
+                    >
+                        Belum punya akun?
+                    </Text>
+
+                    <TouchableOpacity>
+                        <Text
+                            style={{
+                                fontFamily: "Inter-Medium",
+                                fontSize: 13,
+                                color: colors.deepMaroon,
+                                marginLeft: 4,
+                            }}
+                        >
+                            Daftar Sekarang
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAwareScrollView >
+        </SafeAreaView>
     );
 };
 
