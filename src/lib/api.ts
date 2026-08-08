c68baf298954ca3157d5b45de9c3ca72d635b8c0
Gung Nanda <gn.nanda0@gmail.com>
2026-08-08 20:10:41 +0800

    feat: update API and socket URLs to use port 5001

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const API_BASE_URL = "http://10.0.2.2:5000";
+export const API_BASE_URL = "http://10.0.2.2:5001";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 // src/lib/socket.ts
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://10.0.2.2:5000";
+const SOCKET_URL = "http://10.0.2.2:5001";
 
 let socket: Socket | null = null;
 
